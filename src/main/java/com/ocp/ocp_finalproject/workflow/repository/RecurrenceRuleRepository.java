092451e86419ee46187fa136deee54dc04c0d1d6
juhyun <guswnfl5@naver.com>
2026-01-10 23:33:43 +0900

    refacotry : 불필요한 메서드 정리

--- src/main/java/com/ocp/ocp_finalproject/workflow/repository/RecurrenceRuleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ocp.ocp_finalproject.workflow.repository;
-
-import com.ocp.ocp_finalproject.workflow.domain.RecurrenceRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecurrenceRuleRepository extends JpaRepository<RecurrenceRule, Long> {
-}
